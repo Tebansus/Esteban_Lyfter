e4f28e7caa16a36a490de3c7b4bc8d3f1841cf37
Tebansus <mogtebanjim@gmail.com>
2025-05-06 01:32:44 -0600

    Week_1_Backend_Homework

---
+++ Week1_Backend/task_api_methodview.py
@@ -0,0 +1,125 @@
+from flask import Flask, request, jsonify
+import json
+import os
+from flask.views import MethodView
+app = Flask(__name__)
+# Allowed status values
+ALLOWED_STATUS = {"to_do", "in_progress", "done"}
+
+
+# Code to return the list of tasks and filter them based on status if the query parameter is provided
+class TasksListAPI(MethodView):
+    def get(self):
+        filtered_tasks = Todo_list
+        status = request.args.get("status")
+        if status:
+            filtered_tasks = list(
+                filter(lambda task: task["status"].lower() == status.lower(), filtered_tasks)
+            )
+        return jsonify({"tasks": filtered_tasks}), 200
+
+# Function to add a new task to the Todo_list. It ensures the task doesnt have a duplicate identifier and that the status is one of the allowed values.
+# It also checks if the task is in the correct format and returns an error message if not.
+# Also verifies all required fields are present in the request.
+#Then, it appends the new task to the Todo_list and saves it to the JSON file.
+class AddTaskAPI(MethodView):    
+    def post(self):
+        new_task = request.get_json(silent=True)
+        if not isinstance(new_task, dict):
+            return jsonify(error = "Invalid task format, please send JSON object"), 400
+        identifier = new_task.get("identifier")
+        title = new_task.get("title")
+        description = new_task.get("description")
+        status = new_task.get("status")
+        
+        if not identifier or not title or not description or not status:
+            return jsonify(error = "Missing required fields"), 400
+        
+        if any(task["identifier"] == identifier for task in Todo_list):
+            return jsonify(error = "Task with this identifier already exists"), 400
+        
+        if status not in ALLOWED_STATUS:
+            return jsonify(error="Invalid status value, allowed values are: to_do, in_progress and done."), 400
+        
+        Todo_list.append({
+                        
+            "identifier": identifier,
+            "title": title,
+            "description": description,
+            "status": status
+        })
+        with open("Todo_list.json", "w") as file:
+            json.dump(Todo_list, file, indent=4)
+        return (jsonify(message="Task added successfully", tasks=Todo_list), 201)
+
+# Function to delete a task from the list. It checks if the  identifier is provided and if it exists in the list.
+# If the task is found, it removes it from the list and saves the updated list to the JSON file.
+# If the task is not found, it returns an error message.
+class DeleteTaskAPI(MethodView):
+    def post(self):
+        task_identifier = request.get_json(silent=True)
+        if not isinstance(task_identifier, dict):
+            return jsonify({"error": "Invalid task format, please send JSON object"}), 400
+        identifier = task_identifier.get("identifier")
+        if not identifier:
+            return jsonify(error =  "Missing required fields"), 400
+        for task in Todo_list:
+            if task["identifier"] == identifier:
+                Todo_list.remove(task)
+                with open("Todo_list.json", "w") as file:
+                    json.dump(Todo_list, file, indent=4)
+                return jsonify(message="Task deleted successfully", tasks=Todo_list), 200
+            
+        return jsonify(error="Task not found"), 404
+
+# Update function to modify an existing task in the Todo_list. It checks if the identifier is provided and if it exists in the list.
+# If the task is found, it updates the task with the new values provided in the request.
+# Not all fields neeed to be provided, only the ones that need to be updated.
+class updateTaskAPI(MethodView):
+    def post(self):
+        task_data = request.get_json(silent=True)
+        if not isinstance(task_data, dict):
+            return jsonify(error="Invalid task format, please send JSON object"), 400
+        identifier = task_data.get("identifier")
+        if not identifier:
+            return jsonify(error="Missing required fields"), 400
+        for task in Todo_list:
+            if task["identifier"] == identifier:
+                title = task_data.get("title")
+                description = task_data.get("description")
+                status = task_data.get("status")
+                
+                if title:
+                    task["title"] = title
+                if description:
+                    task["description"] = description
+                if status:
+                    if status not in ALLOWED_STATUS:
+                        return jsonify(error="Invalid status value, allowed values are: to_do, in_progress and done."), 400
+                    task["status"] = status
+                with open("Todo_list.json", "w") as file:
+                    json.dump(Todo_list, file, indent=4)
+                return jsonify(message="Task updated successfully", tasks=Todo_list), 200
+
+# Main function to run the Flask application. It checks if the Todo_list.json file exists and loads the tasks from it.
+# If the file does not exist, it initializes an empty Todo_list and starts the application.
+# This version uses the MethodView class to define the API endpoints.
+# It defines the routes for listing, adding, deleting, and updating tasks using the MethodView class. and add_url_rule method.
+if __name__ == "__main__":
+    tasks_view = TasksListAPI.as_view("tasks_list_api")
+    app.add_url_rule("/Tasks_List", view_func=tasks_view, methods=["GET"])
+    tasks_add = AddTaskAPI.as_view("add_task_api")
+    app.add_url_rule("/Add_Task", view_func=tasks_add, methods=["POST"])
+    tasks_delete = DeleteTaskAPI.as_view("delete_task_api")
+    app.add_url_rule("/Delete_Task", view_func=tasks_delete, methods=["POST"])
+    tasks_update = updateTaskAPI.as_view("update_task_api")
+    app.add_url_rule("/Update_Task", view_func=tasks_update, methods=["POST"])
+    if os.path.exists("Todo_list.json"):
+        with open("Todo_list.json", "r") as file:
+            Todo_list = json.load(file)
+        app.run(host="localhost", port=5000, debug=True)
+    else:
+         Todo_list = []
+         app.run(host="localhost", port=5000, debug=True)
+    
+    
